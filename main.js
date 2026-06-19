73952e30ff47ed68d2d434f97ee7a1201f7113b7
usmanhussain00 <usmanhussain12343@gmail.com>
2026-06-19 22:41:29 +0500

    First commit

---
+++ main.js
@@ -0,0 +1,252 @@
+/* =====================================
+   INITIALIZE LIBRARIES
+===================================== */
+
+// Lucide Icons
+lucide.createIcons();
+
+// Highlight.js
+hljs.highlightAll();
+
+// GSAP ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+/* =====================================
+   CUSTOM SVG ICONS
+===================================== */
+const dataCustomIcon = document.querySelectorAll("[data-custom-icon]");
+dataCustomIcon.forEach((icon) => {
+  if (icon.getAttribute("data-custom-icon")) {
+    const request = new XMLHttpRequest();
+    request.open(
+      "GET",
+      "./assets/icons/" + icon.getAttribute("data-custom-icon") + ".svg"
+    );
+    request.setRequestHeader("Content-Type", "image/svg+xml");
+    request.addEventListener("load", (event) => {
+      if (event.target.status === 200) {
+        const response = event.target.responseText;
+        icon.innerHTML = response;
+      }
+    });
+    request.send();
+  }
+});
+
+/* =====================================
+   NAVBAR
+===================================== */
+const navbar = document.getElementById("navbar");
+const routes = document.querySelectorAll("#navbar .nav__routes .route");
+const sections = document.querySelectorAll("section");
+
+window.onscroll = () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add("drop");
+  } else {
+    navbar.classList.remove("drop");
+  }
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 100;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      routes.forEach((route) => {
+        route.classList.remove("active");
+        document
+          .querySelector("#navbar .nav__routes a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+/* ======================================
+   THEME SWITCHER
+====================================== */
+
+const themeCheckbox = document.getElementById("theme-checkbox");
+const storageKey = "theme";
+
+/* Load saved theme */
+const savedTheme = localStorage.getItem(storageKey);
+
+if (savedTheme === "dark") {
+  document.documentElement.setAttribute("data-theme", "dark");
+  themeCheckbox.checked = true;
+} else {
+  document.documentElement.setAttribute("data-theme", "light");
+  themeCheckbox.checked = false;
+}
+
+/* Toggle theme */
+themeCheckbox.addEventListener("change", () => {
+  if (themeCheckbox.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem(storageKey, "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem(storageKey, "light");
+  }
+});
+/* =============== NAVBAR END ================ */
+
+/* =====================================
+   SIDEBAR
+===================================== */
+const menuBtn = document.getElementById("menu__btn");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.getElementById("close-sidebar-btn");
+menuBtn.onclick = () => {
+  sidebar.classList.toggle("visible");
+};
+closeBtn.onclick = () => {
+  sidebar.classList.remove("visible");
+};
+/* =============== SIDEBAR END =============== */
+
+/* =====================================
+   HEADER ANIMATION
+===================================== */
+const odometers = document.querySelectorAll(".odometer");
+setTimeout(() => {
+  odometers.forEach((list) => {
+    let id = list.getAttribute("id");
+    if (id === "experience") {
+      list.innerHTML = 1;
+    }
+    if (id === "project") {
+      list.innerHTML = 15;
+    }
+    if (id === "awards") {
+      list.innerHTML = 0;
+    }
+    if (id === "clients") {
+      list.innerHTML = 15;
+    }
+  });
+}, 4000);
+
+//gsap code
+gsap
+  .timeline({ delay: 0.5 })
+  .from("#header .points", { opacity: 0, y: -30 })
+  .from("#header .me", { opacity: 0, scale: 0.7 })
+  .from(
+    ["#header .user__info .sub__title", "#header .user__info .description"],
+    { opacity: 0, y: 20 }
+  )
+  .from("#header .user__info .title", { opacity: 0, x: -30 })
+  .from("#header .user__info .buttons", { opacity: 0, x: -30 })
+  .from("#header .point", { opacity: 0, x: -30, stagger: 0.5 });
+/* ============== HEADER END ================= */
+
+/* =====================================
+   ABOUT SECTION ANIMATION
+===================================== */
+gsap
+  .timeline({
+    delay: 0.5,
+    scrollTrigger: {
+      trigger: "#about",
+      start: "20% bottom",
+      end: "bottom top",
+    },
+  })
+  .from("#about .box", { opacity: 0, y: 30, stagger: 0.5 });
+/* ===============  ABOUT END ================ */
+
+/* =====================================
+   SERVICES SECTION ANIMATION
+===================================== */
+gsap
+  .timeline({
+    delay: 0.5,
+    scrollTrigger: {
+      trigger: "#services",
+      start: "20% bottom",
+      end: "bottom top",
+    },
+  })
+  .from(
+    [
+      "#services .section__header .sub__title",
+      "#services .section__header .description",
+    ],
+    { opacity: 0, y: 30, stagger: 0.5 }
+  )
+  .from("#services .service", { opacity: 0, y: 30, stagger: 0.5 });
+/* ============== SERVICES END ============== */
+
+/* =====================================
+   PROJECTS SECTION ANIMATION
+===================================== */
+gsap
+  .timeline({
+    delay: 0.5,
+    scrollTrigger: {
+      trigger: "#projects",
+      start: "20% bottom",
+      end: "bottom top",
+    },
+  })
+  .from(
+    [
+      "#projects .section__header .sub__title",
+      "#projects .section__header .description",
+    ],
+    { opacity: 0, y: 30, stagger: 0.5 }
+  )
+  .from("#projects .project", { opacity: 0, y: 30, stagger: 0.5 });
+/* ============== PROJECT END ============= */
+
+/* ============== TESTIMONIALS START ============ */
+new Swiper(".testimonials", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  centerSlides: true,
+  speed: 5000,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    600: { slidesPerView: "auto" },
+  },
+});
+gsap
+  .timeline({
+    delay: 0.5,
+    scrollTrigger: {
+      trigger: "#testimonials",
+      start: "20% bottom",
+      end: "bottom top",
+    },
+  })
+  .from(
+    [
+      "#testimonials .section__header .sub__title",
+      "#testimonials .section__header .description",
+    ],
+    { opacity: 0, y: 30, stagger: 0.5 }
+  )
+  .from("#testimonials .testimonial", { opacity: 0, y: 30, stagger: 0.5 });
+/* ============== TESTIMONIALS END ============ */
+
+/* =====================================
+   CONTACT SECTION ANIMATION
+===================================== */
+gsap
+  .timeline({
+    delay: 0.5,
+    scrollTrigger: {
+      trigger: "#contact",
+      start: "20% bottom",
+      end: "bottom top",
+    },
+  })
+  .from("#contact .box", { opacity: 0, y: 30, stagger: 0.5 })
+  .from("#contact .contact__form", { opacity: 0, x: 30 });
+/* ============ CONTACT END ================== */
